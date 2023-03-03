5a5adb4801093cfea4f4c785cd00e2d0977b26b6
MSAAVEDRA98 <simonsnow98@gmail.com>
2023-03-03 10:34:17 -0300

    Main, Navbar y terminado de login

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { MainComponent } from './main/main.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    MainComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
